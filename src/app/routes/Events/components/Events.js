da326bb14d2b2a00116cb85deae6048cd71e4307
Stephanie Keck <stephaniehkeck@gmail.com>
2017-01-01 16:30:30 -0600

    Experimenting with react-toolbox.

--- src/app/routes/Events/components/Events.js
+++ src/app/routes/Events/components/Events.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
 import styles from './Events.scss';
+import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
+import {Button} from 'react-toolbox/lib/button';
 
 class Events extends React.Component {
 	render () {
-		return (<p>Events</p>);
+		return (<div>
+				<h2>Events</h2>
+				<Card>
+					<CardTitle title="Some Event" subtitle="More info."/>
+					<CardMedia/>
+					<CardActions theme="card">
+						<Button label="Details"/>
+						<Button label="RSVP"/>
+					</CardActions>
+				</Card>
+			</div>);
 	}
 }
 
